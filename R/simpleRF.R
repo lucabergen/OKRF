2f0f90e77107a4fbce908f34d840386da454f979
lucabergen <lucabergen@yahoo.com>
2026-09-04 11:25:17 +0200

    Change arguments, refactor inputs

--- R/simpleRF.R
+++ R/simpleRF.R
@@ -1,163 +1,101 @@
-##' Implements Random Forests (Breiman 2001) with emphasis on simplicity. 
-##' Uses reference classes and only plain \code{R}. 
-##' Not optimized for computation speed. 
-##' Allows rapid prototyping of RF-type algorithms.
-##' 
-##' Unordered factor variables can be handled in different ways. 
-##' Use "ignore" to treat them as ordered in the order of the factor levels. 
-##' With "order_once" and "order_split" they are ordered by their response values. For "order_once" this is done once before the analysis, for "order_split" this is done in each split.
+##' A simple implementation of Output Kernel Random Forests using tree-wise
+##' feature approximations based on the R package \code{simpleRF}.
+##' Uses reference classes and only plain \code{R}.
+##' Not optimized for computation speed.
+##'
+##' Unordered factor variables can be handled in different ways.
+##' Use "ignore" to treat them as ordered in the order of the factor levels.
 ##' With "partition" all 2-partitions of the factor levels are considered for splitting.
-##' 
-##' @title simpleRF
-##' @param formula Object of class \code{formula} or \code{character} describing the model to fit.
-##' @param data Training data of class \code{data.frame}.
+##'
+##' @title simpleOKRF
+##' @param K Gram matrix of targets
+##' @param Phi Feature matrix of targets
+##' @param data Covariate data of class \code{data.frame}.
+##' @param eps Error tolerance of approximation. Default 0.001.
 ##' @param num_trees Number of trees.
 ##' @param mtry Number of variables to possibly split at in each node.
-##' @param min_node_size Minimal node size. Default 1 for classification, 5 for regression, 3 for survival and 10 for probability estimation.
+##' @param min_node_size Minimal node size. Default 5.
 ##' @param replace Sample with replacement. Default TRUE.
-##' @param probability Grow a probability forest. Default FALSE.
-##' @param splitrule Splitrule to use in trees. Default "Gini" for classification and probability forests, "Variance" for regression forests and "Logrank" for survival forests.
-##' @param unordered_factors How to handle unordered factor variables. One of "ignore", "order_once", "order_split" and "partition" with default "ignore".
+##' @param unordered_factors How to handle unordered factor variables. Either "ignore" or "partition" with default "ignore".
 ##' @param num_threads Number of threads used for mclapply, set to 1 for debugging.
-##' @examples 
+##' @examples
 ##' \donttest{
-##' library(simpleRF)
-##' 
-##' # Classification
-##' simpleRF(Species ~ ., iris)
-##' 
-##' # Prediction
-##' train_idx <- sample(nrow(iris), 2/3 * nrow(iris))
-##' iris_train <- iris[train_idx, ]
-##' iris_test <- iris[-train_idx, ]
-##' rf_iris <- simpleRF(Species ~ ., data = iris_train)
-##' pred_iris <- rf_iris$predict(iris_test)
-##' table(iris_test$Species, pred_iris)
+##' TODO: Include new example
 ##' }
-##' 
-##' @author Marvin N. Wright
-##' @references
-##' Breiman, L. (2001). Random forests. Mach Learn, 45(1), 5-32. \cr
+##' @author Luca Bergen
 ##' @import stats
 ##' @export
-simpleRF <- function(formula, data, num_trees = 50, mtry = NULL, 
-                     min_node_size = NULL, replace = TRUE, probability = FALSE, 
-                     splitrule = NULL, unordered_factors = "ignore", 
-                     num_threads = 1) {
-  
-  model.data <- model.frame(formula, data)
-  
-  if (class(model.data[, 1]) == "factor") {
-    if (probability) {
-      treetype <- "Probability" 
-    } else {
-      treetype <- "Classification"
-    }
-  } else if (class(model.data[, 1]) == "numeric") {
-    treetype <- "Regression"
-  } else if (class(model.data[, 1]) == "Surv") {
-    treetype <- "Survival"
-  } else {
-    stop("Unkown response type.")
-  }
-  
+simpleOKRF <- function(K = NULL, Phi = NULL, X,
+                       tol = 1e-3, num_trees = 200, mtry = NULL,
+                       min_node_size = NULL, replace = TRUE,
+                       unordered_factors = "ignore",
+                       num_threads = 1) {
+
   ## Check parameters
+  if (is.null(K) && is.null(Phi)) {
+    stop("Either K or Phi must be specified.")
+  }
+
+  if (!is.null(K) && !is.null(Phi)) {
+    warning("Both K and Phi specified. Only Phi is used.")
+  }
+
+  # TODO: Give informative error messages
+  if (!is.null(Phi)) {
+    stopifnot(
+      is.matrix(Phi),
+      nrow(Phi) > ncol(Phi)
+    )
+  } else {
+    stopifnot(
+      is.matrix(K),
+      nrow(K) == ncol(K)
+    )
+  }
+  stopifnot(
+    length(tol) == 1L,
+    is.finite(tol),
+    tol >= 0
+  )
+
   if (is.null(mtry)) {
-    mtry <- sqrt(ncol(model.data)-1)
-  } else if (mtry > ncol(model.data)-1) {
+    mtry <- sqrt(ncol(X))
+  } else if (mtry > ncol(X)) {
     stop("Mtry cannot be larger than number of independent variables.")
   }
   if (is.null(min_node_size)) {
-    if (treetype == "Classification") {
-      min_node_size <- 1
-    } else if (treetype == "Probability") {
-      min_node_size <- 10
-    } else if (treetype == "Regression") {
-      min_node_size <- 5
-    } else if (treetype == "Survival") {
-      min_node_size <- 3
-    }
+    min_node_size <- 5
   }
-  
-  ## Splitrule
-  if (is.null(splitrule)) {
-    if (treetype == "Classification") {
-      splitrule <- "Gini"
-    } else if (treetype == "Probability") {
-      splitrule <- "Gini"
-    } else if (treetype == "Regression") {
-      splitrule <- "Variance"
-    } else if (treetype == "Survival") {
-      splitrule <- "Logrank"
-    }
-  }
-  
+
   ## Unordered factors
-  if (!(unordered_factors %in% c("ignore", "order_once", "order_split", "partition"))) {
+  if (!(unordered_factors %in% c("ignore", "partition"))) {
     stop("Unknown value for unordered_factors.")
   }
   covariate_levels <- list()
-  
-  if (unordered_factors == "order_once") {
-    ## Reorder factor columns depending on response type
-    model.data <- reorder.factor.columns(model.data)
-    
-    ## Save levels
-    covariate_levels <- lapply(model.data[, -1], levels)
-  }
-  else if (unordered_factors == "ignore") {
+
+  if (unordered_factors == "ignore") {
     ## Just set to ordered if "ignore"
-    character.idx <- sapply(model.data[, -1], is.character)
-    ordered.idx <- sapply(model.data[, -1], is.ordered)
-    factor.idx <- sapply(model.data[, -1], is.factor)
+    character.idx <- sapply(X, is.character)
+    ordered.idx <- sapply(X, is.ordered)
+    factor.idx <- sapply(X, is.factor)
     recode.idx <- character.idx | (factor.idx & !ordered.idx)
-    model.data[, -1][, recode.idx] <- lapply(model.data[, -1][, recode.idx], as.ordered)
-    
+    X[, recode.idx] <- lapply(X[, recode.idx], as.ordered)
+
     ## Save levels
-    covariate_levels <- lapply(model.data[, -1], levels)
+    covariate_levels <- lapply(X, levels)
   }
-    
+
   ## Create forest object
-  if (treetype == "Classification") {
-    forest <- ForestClassification$new(num_trees = as.integer(num_trees), mtry = as.integer(mtry), 
-                                       min_node_size = as.integer(min_node_size), 
-                                       replace = replace, splitrule = splitrule,
-                                       data = Data$new(data = model.data), 
-                                       formula = formula, unordered_factors = unordered_factors, 
-                                       covariate_levels = covariate_levels,
-                                       response_levels = levels(model.data[, 1]))
-  } else if (treetype == "Probability") {
-    forest <- ForestProbability$new(num_trees = as.integer(num_trees), mtry = as.integer(mtry), 
-                                   min_node_size = as.integer(min_node_size), 
-                                   replace = replace, splitrule = splitrule,
-                                   data = Data$new(data = model.data), 
-                                   formula = formula, unordered_factors = unordered_factors,
-                                   covariate_levels = covariate_levels,
-                                   response_levels = levels(model.data[, 1]))
-  } else if (treetype == "Regression") {
-    forest <- ForestRegression$new(num_trees = as.integer(num_trees), mtry = as.integer(mtry), 
-                                   min_node_size = as.integer(min_node_size), 
-                                   replace = replace, splitrule = splitrule,
-                                   data = Data$new(data = model.data), 
-                                   formula = formula, unordered_factors = unordered_factors, 
-                                   covariate_levels = covariate_levels)
-  } else if (treetype == "Survival") {
-    idx.death <- model.data[, 1][, 2] == 1
-    timepoints <- sort(unique(model.data[idx.death, 1][, 1]))
-    forest <- ForestSurvival$new(num_trees = as.integer(num_trees), mtry = as.integer(mtry), 
-                                 min_node_size = as.integer(min_node_size), 
+  forest <- ForestRegression$new(num_trees = as.integer(num_trees), mtry = as.integer(mtry),
+                                 min_node_size = as.integer(min_node_size),
                                  replace = replace, splitrule = splitrule,
-                                 data = Data$new(data = model.data), 
-                                 formula = formula, unordered_factors = unordered_factors, 
-                                 covariate_levels = covariate_levels,
-                                 timepoints = timepoints)
-  } else {
-    stop("Unkown tree type.")
-  }
+                                 data = Data$new(data = model.data),
+                                 formula = formula, unordered_factors = unordered_factors,
+                                 covariate_levels = covariate_levels)
 
   ## Grow forest
   forest$grow(num_threads = num_threads)
 
   ## Return forest
-  return(forest) 
+  return(forest)
 }
